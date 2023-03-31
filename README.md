6c8378f5c18acd13f5b88400fa3aa503693529dc
Brent Houston <brentjustinhouston@gmail.com>
2023-03-30 22:42:26 -0700

    README update

--- README.md
+++ README.md
@@ -1,135 +1,35 @@
-# PasswordGenerator
+# Password Generator
 
+## Table of contents
+* [General info](#general-info)
+* [Links](#links)
+* [Technologies](#technologies)
+* [Resources](#resources)
+* [Deployed App Screenshots](#resources)
 
-//will have to make arrays of lower and upper letters, numbers, and symbols
-//make generatePassword function
-//prompt for password length is a number
-
-
-//if statment if number is above 8 && below 128
-//confirm for lowercase, uppercase, numeric and special characters
-
-
-//store there response in a variable
-
-
-//if statment for each confirm and if it is a yes or true I would push or add random characters to a big array of all characters selected so far.
-
-
-//for loop while i is less than there answer for num length prompt
-
-
-//in this for loop we would randomly pull characters from the big array
-
-
-//pull random characters from the array using math.random 
-
-
-//have a var declared above for loop
-//that var += theRandomChar
-//return the password var
-
-var globalPassword = []
-
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-// console.dir(generateBtn)
-
-function generatePassword(lenght) {
-  askPrompt()
-  return something;
-}
-
-function askPrompt (){
-    //ask user what kind of characters and how long
-     // create an array of characters based on the answers that they gave
-    // add placeholders to global password
-
-}
-
-function passwordBuild (){
-
-    //build the password letter by letter (for..loop) what the user says is the length
-
-    for(let i=0; i<passwordArray.length; i++){
-        var randomNum = Math.floor(Math.random() * availChar.length); //picks a random number, rounds it down and multiplies it by 26
-  var randomLetter = availChar[randomNum];
-    }
-}
+## General info
+This app is a password gererator that will prompt user to customize thier password. It asks user to specify how long the password should be, should it include uppercase, lowercase, numbers and/or special characters. It then randomizes and returns a unique password.
+	
+## Technologies
+Project is created with:
+* JavaScript
+* HTML
+* CSS
 
 
 
-// for(let i = 0; i < charLength;i++)
+## Links
+- [Link to deployed site](https://brenthouston.github.io/PasswordGenerator/)
+- [Link to code repository](https://github.com/brenthouston/PasswordGenerator)
+	
+## Resources
+- [MDN](https://developer.mozilla.org/en-US/)
+- [W3 Schools](https://www.w3schools.com/)
+- [stackoverflow](https://stackoverflow.com/questions/60335967/getting-multiple-random-strings-from-array-of-strings)
+- [Java2Blog](https://java2blog.com/remove-comma-from-string-javascript/#:~:text=Using%20replaceAll()-,To%20remove%20comma%20from%20string%20in%20Javascript%3A,matches%20replaced%20by%20given%20replacement.)
 
-// function charLength () {
-//     var password = 10;
-//     }
-// var charLength = 26;
+## Deployed App Screenshots
 
-// for(let i = 0; i < charLength;i++)
+Loaded on mobile viewport
+![Generate Password](https://github.com/brenthouston/Portfolio/blob/main/Images/deployed%20site%20loaded%20to%20small%20vierport.png)
 
-
-const string = 'word';
-
-<!-- // Option 1
-string.split('');
-
-// Option 2
-[...string];
-
-// Option 3
-Array.from(string);
-
-// Option 4
-Object.assign([], string);
-
-// Result:
-// ['w', 'o', 'r', 'd'] -->
-
-
-retun more than one index in an array
-
-const array1 = ["A", "b", "C", "D", "f"];
-const array2 = ["❤️", "🔥", "👋", "🦑", "😅"];
-const array3 = ["0", "1", "2", "3", "4", "5"];
-const arr = [array1, array2, array3];
-const minimum = 2;
-const arrCount = new Array(arr.length).fill(0);
-function renderItem() {
-  let randArrIndex;
-  do {
-    randArrIndex = Math.floor(Math.random() * arr.length);
-    //get another index if this array already have the minimum number
-    //and there is another array that still didn't reach the minimum
-  } while (arrCount[randArrIndex] >= minimum && arrCount.some((e) => e < minimum));
-
-  //increase the count for the selected array index
-  arrCount[randArrIndex]++;
-
-  //now select a random Item from that array (we need it's length to get a valid index)
-  let randItemIndex = Math.floor(Math.random() * arr[randArrIndex].length);
-  return arr[randArrIndex][randItemIndex];
-}
-
-function generateRandom() {
-  let randomValue = "";
-  //!! the string need to be at least equal to: minimum * arr.length
-  // otherwise you will have an infinite loop
-  for (let i = 0; i < 6; i++) {
-    randomValue += renderItem();
-  }
-  return randomValue;
-}
-console.log(generateRandom());
--- script.js
+++ script.js
@@ -3,116 +3,170 @@ var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
 function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-  
-    passwordText.value = password;
-}
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
+  passwordText.value = password;
+}
 
 var globalPassword = [];
 var userChoicePassPoss = [];
 
-
 //will have to make arrays of lower and upper letters, numbers, and symbols
-var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var upperCase = [ "A","B", "C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var numbArr = ["1","2","3","4","5","6","7","8","9"];
-var symbArr = ["!","@","#","$","%","^","&","*","(",")","-","+","=","~","[","]","?"];
+var lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var numbArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symbArr = [
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "-",
+  "+",
+  "=",
+  "~",
+  "[",
+  "]",
+  "?",
+];
 
+function generatePassword() {
+  var passwordLength = window.prompt(
+    "What is the length of the password you wish to create? Let's make it between 8 and 128 Characters."
+  );
 
-
-function generatePassword(){
- var passwordLength = window.prompt(
-        "What is the length of the password you wish to create? Let's make it between 8 and 128 Characters."
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    window.alert("Ok, let's do this.");
+  } else {
+    window.alert(
+      "Sorry, please make password length a number between 8 and 128."
     );
 
-    if (passwordLength >= 8 && passwordLength <= 128) {
-        window.alert("Ok, let's do this.")
+    window.location.stop();
+  }
 
-     }else{
-        window.alert("Sorry, please make password length a number between 8 and 128.");
+  var pasLowerCase = window.prompt(
+    "Does your password require lower case letters?"
+  );
 
-       window.location.stop();
-       
-    };    
-
-    var pasLowerCase = window.prompt(
-     "Does your password require lower case letters?"
-    );
-
-    if (pasLowerCase == "y") {
+  if (pasLowerCase == "y") {
     userChoicePassPoss = userChoicePassPoss.concat(lowerCase);
-    }else{
-       userChoicePassPoss = userChoicePassPoss;
-    }
+  } else {
+    userChoicePassPoss = userChoicePassPoss;
+  }
 
-    console.log(userChoicePassPoss);
+  console.log(userChoicePassPoss);
 
+  var pasUpperCase = window.prompt(
+    "Does your password require upper case letters?"
+  );
 
-    var pasUpperCase = window.prompt(
-     "Does your password require upper case letters?"
-     );
-   
-    if (pasUpperCase == "y") {
-        userChoicePassPoss = userChoicePassPoss.concat(upperCase);
-    }else{
-        userChoicePassPoss = userChoicePassPoss
-    };
-   console.log(userChoicePassPoss);
+  if (pasUpperCase == "y") {
+    userChoicePassPoss = userChoicePassPoss.concat(upperCase);
+  } else {
+    userChoicePassPoss = userChoicePassPoss;
+  }
+  console.log(userChoicePassPoss);
 
-    var pasNumbArr = window.prompt(
-    "Does your password require numbers?"
-    );
+  var pasNumbArr = window.prompt("Does your password require numbers?");
 
-    if (pasNumbArr == "y") {
-        userChoicePassPoss = userChoicePassPoss.concat(numbArr);
-    }else{
-   userChoicePassPoss = userChoicePassPoss;
-    };
-    console.log(userChoicePassPoss);
+  if (pasNumbArr == "y") {
+    userChoicePassPoss = userChoicePassPoss.concat(numbArr);
+  } else {
+    userChoicePassPoss = userChoicePassPoss;
+  }
+  console.log(userChoicePassPoss);
 
-
-    var pasSymbArr = window.prompt(
+  var pasSymbArr = window.prompt(
     "Does your password require special characters?"
-    );
+  );
 
-    if (pasSymbArr == "y") {
-        userChoicePassPoss = userChoicePassPoss.concat(symbArr);
-    }else{
-        userChoicePassPoss = userChoicePassPoss;
+  if (pasSymbArr == "y") {
+    userChoicePassPoss = userChoicePassPoss.concat(symbArr);
+  } else {
+    userChoicePassPoss = userChoicePassPoss;
+  }
+  console.log(userChoicePassPoss);
+
+  var getPassword = (a, n) => {
+    var l = a.slice();
+    var o = [];
+    for (let i = 0; i < n; i++) {
+      o.push(...l.splice(Math.floor(Math.random() * l.length), 1));
     }
-    console.log(userChoicePassPoss)
+    return o.toString("").replaceAll(",", "");
+  };
 
+  var generateBtn = document.querySelector("#generate");
 
-     var getPassword = (a, n)=>{
-      var l = a.slice()
-      var o = []
-      for (let i = 0; i < n; i++) {
-        o.push(...l.splice(Math.floor(Math.random() * l.length), 1))
-      }
-      return o.toString("");
-    }
-    
-    // password = getPassword(userChoicePassPoss,passwordLength) ;
-    
-    
-    var generateBtn = document.querySelector("#generate");
-    
-    generateBtn.addEventListener("click", writePassword);
-    
-    function writePassword() {
-        var password = getPassword (userChoicePassPoss, passwordLength);
-        var passwordText = document.querySelector("#password");
-        
-        passwordText.value = password;
-    }
-    
-    console.log(getPassword(userChoicePassPoss, passwordLength))
+  generateBtn.addEventListener("click", writePassword);
 
-    
+  function writePassword() {
+    var password = getPassword(userChoicePassPoss, passwordLength);
+    var passwordText = document.querySelector("#password");
 
+    passwordText.value = password;
+  }
+
+  console.log(getPassword(userChoicePassPoss, passwordLength));
 }
-
-
-// Write password to the #password input
